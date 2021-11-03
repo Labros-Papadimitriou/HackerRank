9022ae681feeff753d3bfff0911d8bac54943306
Labros Papadimitriou <87263288+Labros-Papadimitriou@users.noreply.github.com>
2021-11-03 16:05:37 +0200

    Delete symmetric-diference.py

--- Python/symmetric-diference.py
+++
@@ -1,9 +0,0 @@
-m = int(input())
-m_roll = set(map(int, input().split()))
-n = int(input())
-n_roll = set(map(int, input().split()))
-
-total = m_roll.symmetric_difference(n_roll)
-l_total = sorted(list(total))
-for x in l_total:
-    print(x)
